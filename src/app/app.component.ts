80af39c730a37b8315dd774d1d788bcd753c8803
Eswar1266 <86236137+Eswar1266@users.noreply.github.com>
2021-07-03 11:40:25 +0530

    Draft version1 - Connexxion Landing Page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, OnDestroy, OnInit } from '@angular/core';
+import ScrollOut from 'scroll-out';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterContentInit, OnDestroy {
   title = 'connexion-landing';
+  carouselCtx = 1;
+  so: any;
+  ngOnInit() {
+    setInterval(() => {
+      let radioBtn = <HTMLInputElement>document.getElementById(`radio${this.carouselCtx}`);
+      radioBtn.checked =true;
+      this.carouselCtx++;
+      if (this.carouselCtx > 4) {
+        this.carouselCtx = 1;
+      }
+    }, 5000);
+  }
+
+  ngAfterContentInit() {
+    this.so = ScrollOut({
+      targets:'.card,.banner-img,.carousel-content'
+    });
+  }
+
+  ngOnDestroy() {
+    this.so.teardown();
+  }
 }
