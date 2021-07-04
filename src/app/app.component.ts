32e378a3839fdccf05d898592ae56aca5bb28da4
Eswar1266 <86236137+Eswar1266@users.noreply.github.com>
2021-07-05 00:13:26 +0530

    Draft2:  Horizontal scrollable banner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import ScrollOut from 'scroll-out';
 
 @Component({
@@ -6,10 +6,30 @@ import ScrollOut from 'scroll-out';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent implements OnInit, AfterContentInit, OnDestroy {
+export class AppComponent implements OnInit, AfterViewInit, AfterContentInit, OnDestroy {
   title = 'connexion-landing';
   carouselCtx = 1;
   so: any;
+  horzBannerEle: any;
+  horzBannerWrapperEle: any;
+  horzBannerScrollDistance = 0;
+  horzBannerTopOffset = 0
+
+  @HostListener('window:scroll', ['$event']) scrollHandler(e: MouseEvent) {
+    const offsetTop = window.pageYOffset;
+    console.log(offsetTop);
+    if (offsetTop >= this.horzBannerTopOffset && offsetTop <= this.horzBannerScrollDistance + 75) {
+      this.horzBannerWrapperEle.style.transform = "translateX(-" + (offsetTop - this.horzBannerTopOffset) + "px)";
+    } else if (offsetTop < this.horzBannerTopOffset) {
+      this.horzBannerWrapperEle.style.transform = "translateX(0)";
+    } else {
+      this.horzBannerWrapperEle.style.transform = `translateX(-${this.horzBannerScrollDistance -this.horzBannerTopOffset + 75}px)`;
+    }
+  }
+
+  constructor() {
+
+  }
   ngOnInit() {
     setInterval(() => {
       let radioBtn = <HTMLInputElement>document.getElementById(`radio${this.carouselCtx}`);
@@ -21,9 +41,18 @@ export class AppComponent implements OnInit, AfterContentInit, OnDestroy {
     }, 5000);
   }
 
+  ngAfterViewInit() {
+    this.horzBannerEle = <HTMLElement>document.querySelector(".hscrollable-banner");
+    this.horzBannerWrapperEle = <HTMLElement>document.querySelector(".hscrollable-banner__wrapper");
+    this.horzBannerTopOffset = this.horzBannerEle.offsetTop;
+    this.horzBannerScrollDistance = this.horzBannerTopOffset + this.horzBannerWrapperEle.scrollWidth - window.innerWidth;
+    console.log(this.horzBannerTopOffset, this.horzBannerWrapperEle.scrollWidth, window.innerWidth, this.horzBannerScrollDistance);
+    this.horzBannerEle.style.height = this.horzBannerScrollDistance + "px";
+  }
+
   ngAfterContentInit() {
     this.so = ScrollOut({
-      targets:'.card,.banner-img,.carousel-content'
+      targets:'.title-card,.card,.banner-img,.carousel-content'
     });
   }
 
